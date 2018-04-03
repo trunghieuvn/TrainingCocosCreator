61ff13816d2b0d3fc3a67918e266de29728ae612
TranTrungHieu <hieu.trantrung1204@icloud.com>
2018-04-03 18:00:03 +0700

    + final HelloWorldCountTime

--- assets/Script/GameManager.ts
+++ assets/Script/GameManager.ts
@@ -10,41 +10,6 @@
 
 const {ccclass, property} = cc._decorator;
 
-<<<<<<< HEAD
-@ccclass
-export default class GameManager extends cc.Component {
-    
-    @property(cc.Label)  label: cc.Label = null;
-
-    isStart : boolean;
-    totalTime: number = 0;
-    
-    // LIFE-CYCLE CALLBACKS:
-    onLoad () {
-        
-    }
-
-    start () {
-        this.totalTime = 0;
-        this.isStart = false;
-    }
-
-    update (dt) {
-
-        if (this.isStart == false)
-            return;
-
-        if(Math.floor(this.totalTime) > 5)
-            return;
-
-        this.totalTime += dt;
-        this.label.string = Math.floor(this.totalTime).toString();
-    }
-
-    // GUI
-    btnPlay() {
-        this.isStart = true;
-=======
 enum  GameState {
     Start,
     InGame,
@@ -122,7 +87,6 @@ export default class GameManager extends cc.Component {
     
     btnPlay() {
         this.playGame();
->>>>>>> 9fd3c96cd7ca7c0b38579c4eb82e4bde8c437cb4
     }
     btnExit() {
         cc.game.end();
