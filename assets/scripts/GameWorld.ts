e7ef5d984513ca4699abf1ee95d56501190cf540
leviethao <leviethao2@gmail.com>
2019-03-10 09:25:53 +0700

    + createTower, spawnTower dynamic with prefab

---
+++ assets/scripts/GameWorld.ts
@@ -0,0 +1,60 @@
+// Learn TypeScript:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class GameWorld extends cc.Component {
+    @property(cc.Prefab) prefTower: cc.Prefab = null;
+
+    // =========================== Members ================================
+    private headTower: cc.Node = null;
+    private tailTower: cc.Node = null;
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+        this.spawnTower();
+    }
+
+    // update (dt) {}
+
+    // =========================== Methods =================================
+    public createTower (x: number, y:number) :cc.Node {
+        let tower = cc.instantiate(this.prefTower);
+        tower.x = x;
+        tower.y = y;
+        this.node.addChild(tower);
+        return tower;
+    }
+
+    public spawnTower () {
+        // random position
+        let newTower: cc.Node = null;
+        let x, y;
+
+        if (this.tailTower === null) {
+            newTower = this.createTower(-200, -100);
+        } else {
+            if (this.tailTower.x < 0) {
+                x = Math.random() * 50 + 150;
+            } else {
+                x = Math.random() * -50 - 150;
+            }
+            y = Math.random() * 50 + 500;
+
+            newTower = this.createTower(x, y);
+        }
+        this.headTower = this.tailTower;
+        this.tailTower = newTower;
+    }
+}
