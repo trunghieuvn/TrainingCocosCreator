d8cac44d78492f20982d2426b6002b94765e3179
Vu Nguyen <vunguyendev1102@icloud.com>
2019-03-10 16:21:05 +0700

    ok

--- assets/Script/PokemonControll.ts
+++ assets/Script/PokemonControll.ts
@@ -1,3 +1,5 @@
+import PokemonController from "./PokemonController";
+
 // Learn TypeScript:
 //  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
@@ -20,10 +22,38 @@ export default class PokemonControll extends cc.Component {
 
     @property
     text: string = 'hello';
+    total: number  = 0;
+
+    callbackCollider : () => void = null;
+
+    //@property(cc.Node) :cc.Prefab = null;
+
+    //@property(cc.Node) mainscreen:cc.Node = null;
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+    onLoad () {
+        cc.director.getCollisionManager().enabled = true;
+    }
+
+    setCallBackVN(callback){
+        this.callbackCollider = callback;
+    }
+
+    onCollisionEnter(other, self) {
+        cc.log("Ball onCollisionEnter");
+        this.changeDir();
+        this.callbackCollider();
+        //var obj = new cc.Node();
+        //var ball = obj.getComponent(PokemonController);
+        //ball.setPoint(1);
+        //this.total++;
+        //var obj = cc.instantiate(this.node.getPa);
+        //var ball = obj.getComponent(PokemonController);
+        //ball.setPoint(1);
+        //console.log(this.total);
+
+    }
 
     changeDir(){
         this.dir *= -1;
@@ -34,6 +64,8 @@ export default class PokemonControll extends cc.Component {
     }
 
     update (dt) {
-        this.node.y += 100 * dt * this.dir;
+        this.node.y += 500 * dt * this.dir;
+        if(this.node.y<=-250||this.node.y>=250) this.changeDir();
+        //cc.log(this.node.y);
     }
 }
--- assets/Script/PokemonController.ts
+++ assets/Script/PokemonController.ts
@@ -17,21 +17,59 @@ export default class PokemonController extends cc.Component {
     label: cc.Label = null;
 
     @property(cc.Prefab) pokemon:cc.Prefab = null;
-    
+
+    @property(cc.Label) lbTotal:cc.Label;
 
     @property
     text: string = 'hello';
+    total: number = 0;
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        for(var i = 0; i<9; i++){
+        for(var i = 0; i<2; i++){
             var obj = cc.instantiate(this.pokemon);
-            obj.x = -60*i;
+            obj.x = 0;
+            if(i==0){
+                obj.y = -250;
+            }else{
+                obj.y = 250;
+            }
             var ball = obj.getComponent(PokemonControll);
-            if(i % 2 == 0) ball.changeDir();
+            if(i % 2 != 0) ball.changeDir();
             this.node.addChild(obj);
+
+            ball.callbackCollider = this.showTotal.bind(this);
+            
+            /*
+            var obj1 = cc.instantiate(this.totalNumber);
+            
+            obj1.x = 0;
+            if(i==0){
+                obj1.y = -250;
+            }else{
+                obj1.y = 250;
+            }
+            
+            var ball1 = obj.getComponent(PokemonControll);
+            if(i % 2 != 0) ball1.changeDir();
+            this.node.addChild(obj1);
+            */
+
         }
+        
+    }
+
+    showTotal(){
+        this.total++;
+        this.lbTotal.string = this.total.toString();
+        cc.log("show total");
+        /*
+        if(whichball==1) this.total1++;
+        if(whichball==2) this.total2++;
+        this.totalnumber1.string = this.total1.toString();
+        this.totalnumber2.string = this.total2.toString();
+        */
     }
 
     start () {
