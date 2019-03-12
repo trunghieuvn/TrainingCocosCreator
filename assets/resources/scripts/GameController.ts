3c3a174879916b79bc736f9090de3d995f685140
crhien <crhien@lcincom.com>
2019-03-12 14:17:41 +0700

    Init

---
+++ assets/resources/scripts/GameController.ts
@@ -0,0 +1,21 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class GameController extends cc.Component {
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {}
+
+    start () {
+
+    }
+
+    update (dt) {}
+
+
+    //Event
+    btnPlay(){
+        
+    }
+}
